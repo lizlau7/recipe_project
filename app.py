4325060e7c08099158b28154dd322a80f63c55de
Liz Lau <eklau@uiowa.edu>
2021-05-06 07:06:48 -0500

    password encryption

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from bson.objectid import ObjectId
 from flask import Flask, request, render_template, redirect, url_for, session, flash
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from functools import wraps
-#from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-#bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 ############ TO DO #############
 
 # fix it so contributors can edit and delete their own recipes, but admins can edit and delete everyone's recipes
@@ -168,7 +168,7 @@ def login():
 
     if request.method == 'POST':
         user = users.find_one({"email": request.form['username']})
-        if user and user['password']== request.form['password']:
+       if user and bcrypt.check_password_hash(user['password'], request.form['password']):
             user_obj = User(username=user['email'], role=user['role'], id=user['_id'])
             login_user(user_obj)
             next_page = request.args.get('next')
@@ -208,14 +208,14 @@ def update_myaccount(user_id):
     if request.method == 'POST':
         form = request.form
 
-        password = request.form['password']
+        #password = request.form['password']
 
         users.update({'_id': ObjectId(user_id)},
             {
             'first_name': form['first_name'],
             'last_name': form['last_name'],
             'email': form['email'],
-            'password': password,
+           # 'password': password,
             'role': form['role'],
             'date_added': form['date_added'],
             'date_modified': datetime.datetime.now()
@@ -242,7 +242,7 @@ def admin_add_user():
         form = request.form
         
         password = request.form['password']
-      #  pwd_hash=bcrypt.generate_password_hash(password)
+        pwd_hash=bcrypt.generate_password_hash(password)
         
         email = users.find_one({"email": request.form['email']})
         if email:
@@ -252,7 +252,7 @@ def admin_add_user():
             'first_name': form['first_name'],
             'last_name': form['last_name'],
             'email': form['email'],
-            'password': password, 
+            'password': pwd_hash, 
             'role': form['role'],
             'date_added': datetime.datetime.now(),
             'date_modified': datetime.datetime.now()
